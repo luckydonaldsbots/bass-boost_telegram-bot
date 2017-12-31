5a84e702e0eb843cb4efa5e60deda622f37b88d8
luckydonald <luckydonald@flutterb.at>
2017-12-31 16:07:40 +0100

    [bass_boost] Updated translation: DE

--- code/bass_boost/langs/de.py
+++ code/bass_boost/langs/de.py
@@ -7,7 +7,9 @@ class Lang(LBase):
     start_message = "Dieser Bot verstärkt den Bass in Musik die du schickst. Probier's mal mit 'ner Audiodatei."
     help_message = start_message + "\n\nTeil des @luckydonaldsbots Netzwerkes.\n\nDIESER BOT IST SO EIN MÜLL\nJO!!! DIGGR!!! WARUM NUTZT DU DAS?"
     caption = "@{bot} verstärkte grade den Bass!"
-    caption_shit = lambda times: "!!!!111111!!112111!"[:times] + ("" if times < 20 else ("ONE" if times < 21 else "ELEVEN") + "!11!1!111!1!!"[:times-21] + "".join(str(i-33) for i in range(34, times)))
+    capslock_number_1 = "EINS"
+    capslock_number_11 = "ELF"
+    capslock_number_111 = "EINHUNDERTUNDELF"
     progress0 = "Läd Datei herunter"
     progress1 = "Lädt Audio"
     progress2 = "Zerteilung des Tracks"
@@ -16,4 +18,6 @@ class Lang(LBase):
     progress5 = "Berechne stärkere Bassverstärkung"
     progress6 = "Extrahiere zu verstärkende Bereiche"
     progress7 = "Wende den besseren Bass auf die Orginalspur an"
+    generic_error = "Ein Fehler trat auf. Bitte versuche es erneut, sobald dieser behoben ist."
+    task_scheduled = "Deine Datei wurde zur Bassverstärkung eingereiht."
 # end class
